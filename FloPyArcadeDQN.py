bd7125b9562e78e89485f80648f039e1cd92330b
philipphoehn <10443174+philipphoehn@users.noreply.github.com>
2021-02-25 01:58:50 +0100

    Update FloPyArcadeDQN.py

--- FloPyArcadeDQN.py
+++ FloPyArcadeDQN.py
@@ -150,7 +150,7 @@ def main(envSettings, hyParams):
 
     # initializing agent
     agent = FloPyAgent(
-        env.observationsVector,
+        env.observationsVectorNormalized,
         env.actionSpace,
         hyParams,
         envSettings,
