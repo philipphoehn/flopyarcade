03ab37301e7bb69b06dafdea3abee38d7ed54390
philipphoehn <10443174+philipphoehn@users.noreply.github.com>
2021-02-25 01:14:29 +0100

    added argparsing

--- FloPyArcadePlay.py
+++ FloPyArcadePlay.py
@@ -6,6 +6,7 @@
 # philipp.hoehn@yahoo.com
 
 
+from argparse import ArgumentParser
 from FloPyArcade import FloPyArcade, FloPyAgent
 FloPyAgent.suppressTensorFlowWarnings(FloPyAgent)
 from matplotlib.pyplot import switch_backend
@@ -13,27 +14,61 @@ try: switch_backend('TkAgg')
 except: print('Could not import TkAgg as a backend. Visualization may not show.')
 from numpy.random import randint
 
+parser = ArgumentParser(description='FloPyArcade game')
+parser.add_argument('--envtype', default='3s-d', type=str,
+    help='string defining environment')
+parser.add_argument('--seedenv', default=1, type=int,
+    help='integer enabling reproducibility of the environments')
+parser.add_argument('--nlay', default=1, type=int,
+    help='integer defining numbers of model layers')
+parser.add_argument('--nrow', default=100, type=int,
+    help='integer defining grid rows')
+parser.add_argument('--ncol', default=100, type=int,
+    help='integer defining grid columns')
+parser.add_argument('--saveplot', default=False,
+    help='boolean defining whether to save game animation')
+parser.add_argument('--manualcontrol', default=False,
+    help='boolean defining manual control model')
+parser.add_argument('--render', default=True,
+    help='boolean defining if displaying runs')
+parser.add_argument('--modelnameload', default=None,
+    help='string defining model to load')
+parser.add_argument('--modelname', default='FloPyArcade', type=str,
+    help='string defining model basename')
+parser.add_argument('--pathmf2005', default=None,
+    help='string defining local path to MODFLOW 2005 executable')
+parser.add_argument('--pathmp6', default=None,
+    help='string defining local path to MODPATH 6 executable')
+parser.add_argument('--surrogatesimulator', default=None,
+    help='currently unavailable')
+parser.add_argument('--ngames', default=100, type=int,
+    help='integer defining number of games played in a row')
+parser.add_argument('--nagentsteps', default=200, type=int,
+    help='integer defining the number of steps taken per game')
+args = parser.parse_args()
+
+
 # environment settings
 envSettings = {
-    'ENVTYPE': '3',
-    'MODELNAMELOAD': 'ENV3',
-    'MODELNAME': None,
-    'PATHMF2005': None,
-    'PATHMP6': None,
-    'SURROGATESIMULATOR': None,
-    'SAVEPLOT': True,
-    'MANUALCONTROL': True,
-    'RENDER': True,
-    'ENVSEED': 1,
-    'NLAY': 1,
-    'NROW': 100,
-    'NCOL': 100
+    'ENVTYPE': args.envtype,                            # string defining environment
+    'SEEDENV': args.seedenv,                            # integer enabling reproducibility of the environments
+    'NLAY': args.nlay,                                  # integer defining numbers of model layers
+    'NROW': args.nrow,                                  # integer defining grid rows
+    'NCOL': args.ncol,                                  # integer defining grid columns
+    'SAVEPLOT': args.saveplot,                          # boolean defining whether to save game animation
+    'MANUALCONTROL': args.manualcontrol,                # boolean defining manual control model
+    'RENDER': args.render,                              # boolean defining if displaying runs
+    'MODELNAMELOAD': args.modelnameload,                # string defining model to load
+    'MODELNAME': args.modelname,                        # string defining model basename
+    'PATHMF2005': args.pathmf2005,                      # string defining local path to MODFLOW 2005 executable
+    'PATHMP6': args.pathmp6,                            # string defining local path to MODPATH 6 executable
+    'SURROGATESIMULATOR': args.surrogatesimulator       # currently unavailable
 }
 
 # game settings
 gameSettings = {
-    'NGAMES': 1000000000,
-    'NAGENTSTEPS': 200
+    'NGAMES': args.ngames,                              # integer defining number of games played in a row
+    'NAGENTSTEPS': args.nagentsteps                     # integer defining the number of steps taken per game
 }
 
 
